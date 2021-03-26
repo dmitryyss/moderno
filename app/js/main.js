1151941aa302d17e69fc1cbba1aea2c5d42774fb
Миша Потапов <1@gmail.com>
2021-03-26 19:56:41 +0300

    blog slider and first slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
+$(function() {
 
-/* Mixitup */
-var mixer = document.querySelector('.prodcut__box');
-var mix = mixitup(mixer);
+    /* Mixitup */
+    var mixer = document.querySelector('.prodcut__box');
+    var mix = mixitup(mixer);
+
+
+    /* Slider */
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+})
+
+
